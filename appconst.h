3613010ac635aba40f317f5b9266b46594d41998
Fareenakhtar <59418699+Fareenakhtar@users.noreply.github.com>
2020-12-29 22:36:13 +0530

    Delete appconst.h

--- appconst.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPCONSTANT_H_INCLUDED
-#define APPCONSTANT_H_INCLUDED
-#define MAX_DEPTH   32
-#define STACK_OK    1
-#define STACK_EMPTY 2
-#define STACK_FULL  3
-
-
-#endif // APPCONSTANT_H_INCLUDED
